5a68db4a32b063f8480f78608478cc60890c6436
Aman Pal Singh <APS@macoss-MBP.lan>
2024-12-28 13:34:43 +0530

    Initialised and tested Astra DB connection using secure-bundle

--- src/main/java/com/aps/echo/EchoApplication.java
+++ src/main/java/com/aps/echo/EchoApplication.java
@@ -1,15 +1,15 @@
 package com.aps.echo;
 
-//import com.datastax.astra.client.DataAPIClient;
-//import com.datastax.astra.client.Database;
-import com.datastax.astra.client.DataAPIClient;
-import com.datastax.astra.client.Database;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 @RestController
@@ -17,7 +17,12 @@ public class EchoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EchoApplication.class, args);
+    }
 
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
     }
 
 
